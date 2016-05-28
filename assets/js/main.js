f211a89ae6de2cc628abf79c9c5552b74c31a925
Thomas-Ko <tomkolodzi@gmail.com>
2016-05-28 11:24:48 -0400

    Clean up code, add comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,12 @@
-// running : true
+/*====================
+	MODEL 
+====================*/
 
 model = {
 	break : 5,
 	session: 25,
+	
+	//the Current objects keep track of the current timer; seconds must be 0 and minutes must be the same as what is in the model
 	breakCurrent: {
 		minutes: 5,
 		seconds: 0,
@@ -11,24 +15,27 @@ model = {
 		minutes: 25,
 		seconds: 0,
 	},
+
+	//stores which timeType is currently running ("session" or "break"); Do not change this. Initially, it must be set to "session"
 	currentRunning: "session",
+	//stores whether the timer is currently running
 	timerRunning: false,
 };
 
+
+/*====================
+	CONTROLLER 
+====================*/
+
 controller = {
 	init: function(){
-		// controller.countDownClick();
 		view.init();
-		// controller.resetCountDown();
 	},
 
 	addTime : function(timeType){
-		model[timeType]++;
-		// model[timeType+"Current"].minutes = model[timeType];
-		// model[timeType+"Current"].seconds=0;
-		// model.currentRunning = "session";
-		// view.displayInitalSetTimes();
-
+		if(model[timeType]<99){
+			model[timeType]++;
+		}
 		controller.resetTime();
 		model.currentRunning = "session";
 	},
@@ -36,21 +43,16 @@ controller = {
 		if(model[timeType]>1){
 			model[timeType]--;
 		}
-
 		controller.resetTime();
 		model.currentRunning = "session";
-
-		// model[timeType+"Current"].minutes = model[timeType];
-		// model[timeType+"Current"].seconds=0;
-		// model.currentRunning = "session";
-		// view.displayInitalSetTimes();
-
 	},
 
 	retrieveTime: function(timeType){
 		return model[timeType];
 	},
 
+	/*this function starts up the timer; inside this function are other functions that are invoked when the user presses a certain button; These functions
+	need to be inside here because they can't clear the timer out of scope*/
 	countDownClick: function(){
 		controller.resetTime();
 		var timer;
@@ -59,27 +61,25 @@ controller = {
 			timer = window.setInterval(function(){
 				controller.countDown("session", timer);
 			}, 1000);
+			//the class current-time-type is just a lightly opaque container that goes over the currently running time-type section to help the user
 			$("#sessionDisplayContainer").addClass("current-time-type");
 			$("#breakDisplayContainer").removeClass("current-time-type");
-			console.log("RUNNING SESSION");
 		} else if (model.currentRunning==="break"){
 			timer = window.setInterval(function(){
 				controller.countDown("break", timer);
 			}, 1000);
 			$("#breakDisplayContainer").addClass("current-time-type");
 			$("#sessionDisplayContainer").removeClass("current-time-type");
-			console.log("RUNNING BREAK");
 		}
 
-
+		//pressing any of these buttons, stops the timer
 		$("#stop,  #subtractSessionBtn, #addBreakBtn, #subtractBreakBtn, #addSessionBtn").on("click", function(){
 			window.clearInterval(timer);
 			model.timerRunning = false;
 		});
 
-		
+		//pressing the start button, activates the timer again
 		$("#start").on("click", function(){
-		// window.clearInterval(timer);
 			if(!model.timerRunning){
 				window.clearInterval(timer);
 				timer = window.setInterval(function(){
@@ -88,7 +88,6 @@ controller = {
 				model.timerRunning = true;
 				$("#breakDisplayContainer").removeClass("current-time-type");
 				$("#sessionDisplayContainer").addClass("current-time-type");
-				// controller.countDownClick();
 			}
 		});
 
@@ -99,71 +98,42 @@ controller = {
 			controller.resetTime();
 			model.currentRunning = "session";
 			view.displayInitalSetTimes();
-
-			
-			// timer = window.setInterval(function(){
-			// 	controller.countDown("session", timer);
-			// }, 1000);
 		});
 
 	},
 
-	stopCountDown: function(){
-		window.clearInterval(function(){
-			controller.startCountDown("session");
-		});
-	},
-
+	//resets the time
 	resetTime: function(){
 		model.timerRunning = false;
-		// model.currentRunning = "session";		/*CHANGE THIS*/
-
 		model.sessionCurrent.seconds = 0;
 		model.breakCurrent.seconds = 0;
-
 		model.breakCurrent.minutes = model.break;
 		model.sessionCurrent.minutes = model.session;
 		view.displayInitalSetTimes();
-
 	},
 
-
+	//invoked inside the controller.CountDownClick function, this is the brains of the countdown
 	countDown: function(timeType, timer){
-		// var elem = document.getElementById("breakSecondsDisplay");
-		// var currentDisplayTime = parseFloat(countdown.textContent);
-		
-		// elem.textContent = currentDisplayTime - 1;
-
-		// currentSeconds = model.currentBreak.seconds;
-
 		var current = timeType+"Current";
 		if(model[current].seconds>0){
 			model[current].seconds --;
 
 		} else if (model[current].seconds===0){
-			model[current].seconds=2;
+			model[current].seconds=59;
 			model[current].minutes--;
 		}
 
 		var minutesDisplay= "#"+timeType+"Display";
 		var secondsDisplay= "#"+timeType+"SecondsDisplay";
 
-		//when the time reaches 0 
+		//when the timer reaches 0
 		if (model[current].minutes===0 && model[current].seconds===0){
-			console.log("hello");
 			
+			//switch to other timeType countdown
 			if (model.currentRunning==="session"){
-				model.currentRunning="break";
-				
-
-				// $("#breakDisplayContainer").show();
-				// $("#sessionDisplayContainer").hide();
-				
+				model.currentRunning="break";	
 			} else if (model.currentRunning==="break"){
 				model.currentRunning="session";
-				// $("#breakDisplayContainer").hide();
-				// $("#sessionDisplayContainer").show();
-
 			}
 			window.clearInterval(timer);
 			controller.countDownClick();
@@ -173,28 +143,37 @@ controller = {
 			$(secondsDisplay).text("00");
 			$(minutesDisplay).text(model[timeType]);
 
+			//a metal gong plays
 			var audio = document.getElementById("audio");
 			audio.play();
+
 		} else {
-
-		$(secondsDisplay).text(model[current].seconds);
-		$(minutesDisplay).text(model[current].minutes);
-		}
-		
+			var secondsNumber = model[current].seconds;
+			//numbers 0 - 9 will appear with a zero before them; example: 9 becomes 09
+			if(secondsNumber>=0 && secondsNumber<=9){
+				secondsNumber = "0" + secondsNumber;
+			}
+			$(secondsDisplay).text(secondsNumber);
+			$(minutesDisplay).text(model[current].minutes);
+		}	
 	}
-
 };
+
+
+/*====================
+	VIEW 
+====================*/
 
 view = {
 	init: function(){
 		this.displayInitalSetTimes();
-		// this.buttons.addBreakBtn();
 		this.break.addTime();
 		this.break.subtractTime();
 		this.session.addTime();
 		this.session.subtractTime();
 		this.start();
 	},
+
 	displayInitalSetTimes: function(){
 		var breakTime = controller.retrieveTime("break");
 		var sessionTime = controller.retrieveTime("session");
@@ -204,10 +183,7 @@ view = {
 		$("#sessionDisplay").text(sessionTime);
 		$("#sessionSecondsDisplay").text("00");
 		$("#breakSecondsDisplay").text("00");
-
-
 	},
-
 
 	setTime: function(timeType, addOrMinus, func){
 		var btnId = "#" + addOrMinus + timeType.charAt(0).toUpperCase() + timeType.slice(1) +"Btn";
@@ -220,7 +196,6 @@ view = {
 
 			$("#breakDisplayContainer, #sessionDisplayContainer").removeClass("current-time-type");
 		});
-
 	},
 
 	break: {
@@ -244,34 +219,17 @@ view = {
 	start: function(){
 		if(model.timerRunning===false){
 			model.timerRunning = true;
+			//the below is a sort of hackish way I got around being able to have the timer not start right after a page load
 			$("#startBeginning").one("click", function(){
-				//the below is a sort of hackish way I got around being able to not have the timer start right after a page load
 				$("#startBeginning").attr("id","start");
 				controller.countDownClick();
 			});
-
-			
 		}
 	}
-
-	// buttons: {
-	// 	addBreakBtn : function(){
-	// 		$("#addBreakBtn").on("click",function(){
-	// 			controller.addTime("break");
-	// 			var time = controller.retrieveTime("break");
-	// 				console.log(time);
-	// 			$("#breakLength").text(time);
-	// 		});
-	// 	},
-	// 	addSessionBtn : function(){
-	// 		$("#addSessionBtn").on("click",function(){
-	// 			controller.addTime("session");
-	// 			var time = controller.retrieveTime("session");
-	// 				console.log(time);
-	// 			$("#sessionLength").text(time);
-	// 		});
-	// 	},
-	// }
 };
 
+
+/*====================
+	INITIALIZATION 
+====================*/
 controller.init();
